1811c72b9280a11562d40eae448309e1eb10ebe7
Matthew <faceincake@gmail.com>
2022-04-16 14:16:01 -0400

    Short setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+    name="ai-project-final",
+    license="GNU GENERAL PUBLIC LICENSE",
+    url="https://github.com/dpatularu/ai-final-project",
+    description="AI Project for Comp 3710",
+    install_requires=[
+        'pandas',
+        'seaborn',
+        'sklearn'
+    ]
+)
